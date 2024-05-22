ba937a2c901e9159da9cd46fece54e3118adc1d4
Ricardo-UPV <rrommar@upv.edu.es>
2024-05-22 12:03:45 +0200

    Transicion de objeto a array
    He cambiado de trabajar con un objeto con la posicion como llave a un array y utilizar el indice como posicion comoo he estado haciendo en los otros arrays. Tambien he emepzado a modular esta parte nueva del codigo en distintas funciones

--- http/HTML/HUD/RANKING/JS_RANK/pilotos.js
+++ http/HTML/HUD/RANKING/JS_RANK/pilotos.js
@@ -2,6 +2,10 @@
 // Funciones de poner y quitar pilotos             //
 ////////////////////////////////////////////////////
 
+// Array de tiempos global
+tiempos = []
+
+
 // Funciones para crear los informativos del ranking
 // Tiempo
 function crearTiempo(tiemp){
@@ -35,8 +39,14 @@ function sumaPiloto(estado) {
     // Coger el peso
     peso = document.getElementById("peso_input_id").value + "Kg"
 
+    // Encontrar en que posicion va a estar
+    pos = sacar_pos_avion(tiempo)
+
     //Poner el avion en el ranking
-    let nueva_fila = creaFila(piloto,++controla_pilotos, tiempo,peso);
+    let nueva_fila = (piloto,pos, tiempo,peso);
+
+    // Incrementamos la cantidad de aviones que hay
+    ++controla_pilotos
     // El ++ va delante para que no se genere dos veces el nº 1
 
     // Metemos en el ranking
@@ -132,10 +142,12 @@ function subir_posiciones(pos){
     for (fila of filas){
         // Estas son las filas de la izquierda
         let posicion = fila.querySelector('.numero');
-        if (Number(posicion.textContent) > pos){
-            posicion.textContent = Number(posicion.textContent)-1
+
+        // Posicion fila analizando > pos fila a meter
+        if (Number(posicion.textContent) >= pos){
+            posicion.textContent = Number(posicion.textContent)
             // Actualizar el ID de la fila del ranking de todos los de abajo
-            fila.id = String(Number(fila.id)+1)
+            fila.id = String(Number(fila.id))+1
         }
     }
 }
@@ -169,6 +181,8 @@ function cogerTiempo(que){
 }
 
 
+// Version con objeto
+/*
 function meter_en_ranking(nueva_fila) {
     // Funcion para extraer partes del tiempo de la fila nueva que vamos a meter
     function convertirTiempo(tipo) {
@@ -206,7 +220,7 @@ function meter_en_ranking(nueva_fila) {
         let minutos = parseInt(partes[0], 10) * 60;
         let segundos = parseInt(partes[1], 10);
         let miliseg = parseInt(partes[2], 10) * 0.001;
-        console.log("Fila nº: " + fila.id)
+        
         console.log("Miutos " + minutos + " Segundos: " + segundos + " miliseg: " + miliseg)
         
         let tiempo_en_seg = minutos + segundos + miliseg;
@@ -221,7 +235,7 @@ function meter_en_ranking(nueva_fila) {
             2: TIEMPO EN SEG
             ... }
 
-        */
+        
     });
 
     console.log("Objeto Tiempos: ")
@@ -249,6 +263,7 @@ function meter_en_ranking(nueva_fila) {
         
         // Insertar nueva fila antes de la fila existente
         filaExistente.parentNode.insertBefore(nueva_fila, filaExistente);
+        subir_posiciones(claveEncontrada)
         
     } else { // Si NO hay una posición con tiempo menor que el de nuestro avión
         console.log(`Insertar ${nueva_fila.id} al final`);
@@ -257,4 +272,106 @@ function meter_en_ranking(nueva_fila) {
     }
 
     
+}
+*/
+
+
+// Sacar la nueva posicion  
+function sacar_pos_avion(tiempo){
+        // Funcion para extraer partes del tiempo de la fila nueva que vamos a meter
+        function convertirTiempo(tipo) {
+            //let tiempo = nueva_fila.querySelector('.tiempo').textContent;
+            // Partimos en 3 partes ya que esta en MM:SS:XXX
+            let partes = tiempo.split(':');
+            if (tipo === "min") return Number(partes[0])*60;
+            if (tipo === "seg") return Number(partes[1]);
+            if (tipo === "mil") return Number(partes[2])*0.001;
+        }
+    
+        // Convertir el tiempo de nueva_fila a segundos para poder compararlo con el resto
+        let minutos_avion = convertirTiempo("min")
+        let segundos_avion = convertirTiempo("seg")
+        let miliseg_avion = convertirTiempo("mil")
+        let tiempo_avion = minutos_avion + segundos_avion + miliseg_avion; // Tiempo en segundos
+    
+        // let tiempos = []; AHORA ES GLOBAL
+        // Convertir HTMLCollection a un array para poder iterar
+        filas_array = Array.from(filas); 
+
+        // Comprobar el tiempo del resto
+        filas_array.forEach(fila => {
+            // Cogemos el div resto y el div del tiempo
+            let resto = fila.querySelector('.resto');
+            let tiempo = resto.querySelector('.tiempo');
+            
+            // Esto es el tiempo en formato MM:SS:XXX
+            let tiempoText = tiempo.textContent;
+    
+            // Separamos el texto en partes
+            let partes = tiempoText.split(':');
+            // Convertimos cada parte a un número
+            let minutos = parseInt(partes[0], 10) * 60;
+            let segundos = parseInt(partes[1], 10);
+            let miliseg = parseInt(partes[2], 10) * 0.001;
+            
+            console.log("Miutos " + minutos + " Segundos: " + segundos + " miliseg: " + miliseg)
+            
+            let tiempo_en_seg = minutos + segundos + miliseg;
+    
+            // Guardamos en el objeto tiempos el tiempo en segundos en la posición
+            tiempos.push(tiempo_en_seg)
+    
+            /*
+            Est nos deja un array  asi:
+            tiempos = [TIEMPO_EN_SEG,_TIEMPO_EN_SEG]
+                ... 
+    
+            */
+        });        
+        console.log("Tiempo de nuestro avion "+tiempo_avion)
+     
+        // Agregar el tiempo del avión a la lista de tiempos
+        tiempos.push(tiempo_avion);
+    
+        // Ordenar la lista de tiempos
+        let tiempos2 = [...tiempos].sort((a, b) => a - b); // Creamos una instancia nueva de tiempos y que los pequeños vayan delante
+    
+        // Encontrar la nueva posición del tiempo_avion
+        let nueva_posicion = tiempos2.indexOf(tiempo_avion)+1;
+        // CUIADADO, nueva_posicion esta cogiendo indexes de arrays entonces hay que +1
+    
+        return nueva_posicion
+}
+
+// Version con array
+function meter_en_ranking(nueva_fila) {
+    let tiempo = nueva_fila.querySelector('.tiempo').textContent;
+    let pos = nueva_fila.querySelector(".numero").textContent;
+
+    // Comprobar si la posicion ya esta ocupada
+    for (fila in filas){
+        if(fila.id == pos){ // Hay conicidencia
+            subir_posiciones(pos)
+            // Hacer funcion de partir filas e introducir la fila nueva y rejuntar todo filas
+            break;
+        }else{ // No hay, al fondo
+            ranking.append(nueva_fila)
+            return;
+        }
+    }
+    /*
+    // Identificar las posiciones que cambiaron
+    let posiciones_cambiadas = [];
+    tiempos2.forEach((tiempo, index) => {
+        if (tiempos[index] !== tiempo) {
+            posiciones_cambiadas.push(index+1);
+            // CUIADADO, posiciones_cambiadas esta cogiendo indexes de arrays entonces hay que +1
+        }
+    });*/
+
+    
+    console.log(posiciones_cambiadas)
+    // Actualizar IDs de filas
+    // 
+
 }
