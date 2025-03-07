627a0a6535e1f8b154a2de85e627dd035aa45137
DELL_AVidal <a.vidal.ripoll@gmail.com>
2025-03-07 20:16:36 +0100

    Arranque NGSI para tests comprobado

--- tests/start_ngsi.bat
+++ tests/start_ngsi.bat
@@ -1,4 +1,16 @@
 @echo off
-cd ..\docker\orion
+setlocal
+set loc=0
+
+cd ..\docker\orion && set loc=1
+IF %loc% EQU 1  ( goto start )
+
+echo Command executed in wrong directory (\tests)
+goto :eof
+
+:start
+echo.
 echo Starting Orion NGSI server on localhost:1026
+echo.
 docker-compose up
+cd ..\..\tests
--- tests/stop_ngsi.bat
+++ tests/stop_ngsi.bat
@@ -1,4 +1,14 @@
 @echo off
-cd ..\docker\orion
-echo Stopping Orion NGSI server
-docker-compose down
+setlocal
+set loc=0
+
+cd ..\docker\orion && set loc=1
+IF %loc% EQU 1  ( goto stop )
+
+echo Command executed in wrong directory (\tests)
+goto :eof
+
+:stop
+docker-compose rm -fsv
+cd ..\..\tests
+echo Orion NGSI server Stopped
