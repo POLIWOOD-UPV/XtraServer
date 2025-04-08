02afff254ce2981261b4cf3da15df8648bca4113
R.Roman <luigiverdeyt@gmail.com>
2025-04-08 18:34:18 +0200

    Separado express de servidor
    He separado la parte de express de server.js para poder hacer cambios en esta parte sin tener que reiniciar el servidor. Ademas, he corregido un bug que ha surgido ya que ha cambiado los directorios

---
+++ server/modules/app.js
@@ -0,0 +1,78 @@
+//  app.js
+const express = require("express");
+
+// Archivos
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+
+// Modulos
+const dir = require("./dir");
+const { http_logger } = require("./log");
+
+const app = express()
+
+// SUBIR ARCHIVOS
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, dir.DIR+'/uploads/');  // donde se guardan los archivos
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  const upload = multer({ storage: storage });
+  
+// Ruta upload
+app.post('/upload', upload.single('file'), (req, res) => {
+    http_logger.log(req);
+    console.log(req.file);
+    res.send("<p>Archivo subido correctamente</p><br><a href='/subir.html'>Subir otro archivo</a><br><a href='/'>Volver al inicio</a>");
+  });
+
+
+// APLICACION
+// Servir ficheros estaticos
+app.use(express.static(__dirname));
+
+// Ruta NGSI
+app.post('/ngsi', (req, res) => {
+  http_logger.log(req);
+  console.log("NGSI RECIVED");
+  res.status(202).send("No Data");
+});
+
+// Ruta general -> listdir
+app.use((req, res) => {
+  http_logger.log(req);
+  try {
+    const urlPath = path.normalize(req.path); // para trabajar los archivos
+    const fullPath = path.join(__dirname, '..', dir.DIR, urlPath);
+    // Es algo de public?
+    if (!fullPath.startsWith(path.join(__dirname, '..', dir.DIR))) {
+      console.log("2")
+      return res.status(403).send(`Fuera de la carpeta ${dir.DIR}!`);
+    }
+    
+    // Existe?
+    if (fs.existsSync(fullPath)) {
+      // Directorio -> listdirs
+      if (fs.statSync(fullPath).isDirectory()) {
+        dir.http_listdir(res, urlPath);
+      // Archivo -> Servir
+      } else {
+        res.sendFile(fullPath);
+      }
+    // No existe
+    } else {
+      res.status(404).send("404 Not Found");
+    }
+  } catch (err) {
+    console.log("7")
+    console.error("Error en middleware:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+module.exports = app
--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
-/*
-Servidor basado en mi proyecto final de Intera mezclado con el de Xtra2 del anyo pasado
-*/
-// Web
-const express = require("express");
+// Servidor para XC25
+// Conexiones
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -11,18 +8,15 @@ const dir = require('./modules/dir');
 const socket_events = require("./modules/socket_events");
 const { http_logger } = require('./modules/log');
 const ngsi = require('./modules/ngsi');
-
-// Archivos
-const path = require("path");
-const fs = require("fs");
-const multer  = require('multer');
+const app = require("./modules/app")
 
 // Inicializar servidor y app
 const PORT = 80;
-const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
+
+// Sockets
 io.on("connection", (socket) => {
   socket_events.connect(socket);
   console.log("Conexion desde:", socket.conn.id);
@@ -33,67 +27,6 @@ io.on("connection", (socket) => {
     console.log(`Mensaje ${msj} de ${socket.conn.id}`)
     socket_events.events["message"](socket, msj);
   });
-});
-
-
-// Subir archivos
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, dir.DIR+'/uploads/');  // donde se guardan los archivos
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname + path.extname(file.originalname));
-  }
-});
-const upload = multer({ storage: storage });
-
-// APLICACIÓN
-// Mandar archivos estáticos a los clientes
-app.use(express.static(__dirname));
-
-// Ruta para subir archivos
-app.post('/upload', upload.single('file'), (req, res) => {
-  http_logger.log(req);
-  // req.file contiene la información del archivo subido
-  console.log(req.file);
-  res.send("<p>Archivo subido correctamente</p><br><a href='/subir.html'>Subir otro archivo </a><br><a href='/'>Volver al inicio </a>");
-});
-
-app.post('/ngsi', (req, res) => {
-  http_logger.log(req);
-  console.log("NGSI RECIVED");
-  res.status(202).send("No Data");
-});
-
-// Servir directorios y archivos
-app.use((req, res) => {
-  http_logger.log(req);
-  try {
-    const urlPath = path.normalize(req.path); // para trabajar los archivos
-    const fullPath = path.join(__dirname, dir.DIR, urlPath); // para hacer comprobaciones
-
-    // ¿Nos está pidiendo algo de http/?
-    if (!fullPath.startsWith(path.join(__dirname, dir.DIR))) {
-      return res.status(403).send(`Fuera de la carpeta ${dir.DIR}!`);
-    }
-    
-    // Existe?
-    if (fs.existsSync(fullPath)) {
-      // Directorio -> listdirs
-      if (fs.statSync(fullPath).isDirectory()) {
-        dir.http_listdir(res, urlPath);
-      // Archivo -> Servir
-      } else {
-        res.sendFile(fullPath);
-      }
-    // No existe
-    } else {
-      res.status(404).send("404 Not Found");
-    }
-  } catch (err) {
-    console.error("Error en middleware:", err);
-    res.status(500).send("Internal Server Error");
-  }
 });
 
 // Servidor HTTP
