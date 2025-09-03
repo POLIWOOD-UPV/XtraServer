ea2251cd1913c433b2b82059923efc73cab5755f
DELL_AVidal <a.vidal.ripoll@gmail.com>
2025-09-03 21:27:53 +0200

    Fin XC2025

--- old_server/docker_run.bat
+++
@@ -1,4 +0,0 @@
-docker run -it --rm --name xtraserver ^
---mount type=bind,src="%cd%",dst=/home/app,readonly=false ^
--p 80:80 avr24/xtraserver:v1.0 ^
-node server.js
--- old_server/modules/hash.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-exports.hex = (str, type = "md5") => {
-    let hash = crypto.createHash('md5');
-    hash.update(str);
-    let ret = hash.digest("hex");
-    return ret;
-}
