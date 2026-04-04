2471dce080541dadf99e6afe44e2ffaf6a81865f
Gonvita <gvidtar@upv.edu.es>
2026-04-04 19:57:54 +0200

    Anuncios.js show/hide

---
+++ server/public/js/anuncios.js
@@ -0,0 +1,21 @@
+// Declaramos el valor que va a hacer que cambie el evento (True/False)
+let showMainBlock = true;
+
+// Cogemos los elementos del HTML
+const button = document.getElementById("up_down_button");
+const main_block = document.getElementById("contenedor_anuncios");
+
+// Evento para el botón, se activa al hacer click
+button.addEventListener("click", function () {
+    showMainBlock = !showMainBlock;
+
+    // Si showMainBlock es true, el bloque vuelve a la posición original
+    if (showMainBlock) {
+        main_block.style.transform = "translateY(0px)";
+    }
+
+    // Si showMainBlock es false, se oculta el bloque hacia arriba
+    else {
+        main_block.style.transform = "translateY(-100%)";
+    }
+});
