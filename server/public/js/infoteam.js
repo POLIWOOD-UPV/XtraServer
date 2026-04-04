867da6b9536aa0291a416cf14700f7de6879229d
Gonvita <gvidtar@upv.edu.es>
2026-04-04 19:58:25 +0200

    infoteam.js show/hide

---
+++ server/public/js/infoteam.js
@@ -0,0 +1,21 @@
+// Declaramos el valor que va a hacer que cambie el evento (True/False)
+let showMainBlock = true;
+
+// Cogemos los elementos del HTML
+const button = document.getElementById("up_down_button");
+const main_block = document.querySelector(".bloque_info_extra");
+
+// Evento para el botón, se activa al hacer click
+button.addEventListener("click", function () {
+    showMainBlock = !showMainBlock;
+
+    // Si showMainBlock es true, el bloque vuelve a la posición original
+    if (showMainBlock) {
+        main_block.style.transform = "translateX(0px)";
+    }
+
+    // Si showMainBlock es false, se oculta el bloque hacia la izquierda
+    else {
+        main_block.style.transform = "translateX(-100%)";
+    }
+});
